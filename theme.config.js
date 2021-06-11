23e1e8872c4cd02918652d9d99ddfd061d7e6aa2
Aiden Bai <mail.contraband@gmail.com>
2021-06-11 11:09:05 -0700

    Add million brand

--- theme.config.js
+++ theme.config.js
@@ -1,23 +1,23 @@
 export default {
-  repository: 'https://github.com/aidenybai/docs', // project repo
-  docsRepository: 'https://github.com/aidenybai/docs', // docs repo
+  repository: 'https://github.com/millionjs/docs', // project repo
+  docsRepository: 'https://github.com/millionjs/docs', // docs repo
   branch: 'master', // branch of docs
   path: '/', // path of docs
-  titleSuffix: ' – Aiden Bai',
+  titleSuffix: ' – Million',
   nextLinks: true,
   prevLinks: true,
   search: true,
   customSearch: null, // customizable, you can use algolia for example
   darkMode: true,
-  footer: false,
-  footerText: '© 2021 Aiden Bai',
+  footer: true,
+  footerText: '© 2021 Aiden Bai, William Lane',
   footerEditOnGitHubLink: true, // will link to the docs repo
   logo: <>
-    <span><b>Documentation</b> by Aiden Bai</span>
+    &nbsp;<span><img style={{ height: '30px' }} src={'https://raw.githubusercontent.com/millionjs/million/main/.github/assets/logo.svg'} /></span>
   </>,
   head: <>
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="description" content="Documentation" />
-    <meta name="og:title" content="Aiden Bai" />
+    <meta name="description" content="<1kb virtual DOM - it's fast!" />
+    <meta name="og:title" content="Million" />
   </>
 }
