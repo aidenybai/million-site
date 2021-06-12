3da82e4aadbfcfa11d573f74d3fd462f6c6739a6
Aiden Bai <mail.contraband@gmail.com>
2021-06-11 23:03:01 -0700

    New build dir

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 const withNextra = require('nextra')('nextra-theme-docs', './theme.config.js');
 const withImages = require('next-images');
 module.exports = {
+  distDir: 'build',
   target: 'serverless',
   ...withImages(),
   ...withNextra(),
