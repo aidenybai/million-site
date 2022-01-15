e79a3f0ce62603cd4cc1b9d77e31dbb457ac25fb
Aiden Bai <aiden.bai05@gmail.com>
2022-01-15 14:57:21 -0800

    regen

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const withNextra = require('nextra')({
   theme: 'nextra-theme-docs',
   themeConfig: './theme.config.js',
   unstable_contentDump: true,
-  unstable_staticImage: true,
 });
 module.exports = withNextra({
   i18n: {
