58a3a9d9d0c7ae83b03d12ac0694902c2e105a19
Aiden Bai <aiden.bai05@gmail.com>
2022-01-15 15:34:40 -0800

    regen

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const withNextra = require('nextra')({
 });
 module.exports = withNextra({
   i18n: {
-    locales: ['en-US', 'zh-CN'],
+    locales: ['en-US'],
     defaultLocale: 'en-US',
   },
 });
