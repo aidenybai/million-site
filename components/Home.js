666196a9f0d01d8005c1da81279349ba1f8da360
Aiden Bai <aiden.bai05@gmail.com>
2022-01-20 22:21:22 -0800

    bold

--- components/Home.js
+++ components/Home.js
@@ -67,7 +67,8 @@ export default function Page() {
           </span>{' '}
         </h1>
         <p className="max-w-lg mx-auto mt-6 text-3xl font-medium leading-tight text-center text-gray-400 sm:max-w-4xl sm:text-2xl md:text-3xl lg:text-4xl">
-          Million is a lightweight, compiler-augmented virtual DOM. It's fast!
+          Million is a <b>lightweight (&lt;1kb)</b>, compiler-augmented virtual
+          DOM. It's fast!
         </p>
         <div className="max-w-xl mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
           <div className="rounded-md">
@@ -109,8 +110,8 @@ export default function Page() {
           Why Million?
         </p>
         <p className="mx-auto mt-4 text-lg font-medium text-gray-400 lg:max-w-3xl lg:text-xl lg:text-center">
-          Million is designed to be modern and easy to use while being lean and
-          insanely performant.
+          Million is designed to be <b>modern and easy</b> to use while being{' '}
+          <b>lean and insanely performant</b>.
         </p>
         <div className="grid grid-cols-1 mt-12 gap-x-6 gap-y-12 sm:grid-cols-2 lg:mt-16 lg:grid-cols-3 lg:gap-x-8 lg:gap-y-12">
           {features.map((feature) => (
