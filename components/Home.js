cdd4f08e7a0a94fd94010c4cc51ee961c3a154dc
Aiden Bai <aiden.bai05@gmail.com>
2022-03-08 11:16:02 -0800

    Add colored icons

--- components/Home.js
+++ components/Home.js
@@ -94,54 +94,56 @@ export default function Page() {
         videoId="28SMGi-6mNc"
         onClose={() => setOpen(false)}
       />
-      <div className="px-4 pt-20 pb-8 sm:px-6 sm:pt-24 lg:px-8 dark:text-white">
-        <h1 className="text-center text-6xl font-extrabold tracking-tighter leading-[1.1] sm:text-7xl lg:text-8xl xl:text-8xl">
-          Virtual DOM
-          <br />
-          <br className="hidden lg:block" />
-          <span className="future inline-block text-transparent bg-clip-text bg-gradient-radial from-color2 to-color2 via-color1 animate-gradient-x bg-repeat">
-            into the future.
-          </span>{' '}
-        </h1>
-        <p className="max-w-lg mx-auto mt-6 text-3xl leading-tight text-center text-gray-400 sm:max-w-3xl sm:text-3xl md:text-3xl lg:text-3xl">
-          Million is a lightweight, compiler-augmented virtual DOM.
-        </p>
-        <div className="max-w-xl mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
-          <div className="rounded-md">
-            <button
-              onClick={() => setOpen(true)}
-              className="flex items-center justify-center w-full px-8 py-3 text-md text-white no-underline border border-transparent rounded-md bg-gradient-radial from-color2 to-color2 via-color1 animate-gradient-x bg-repeat dark:text-black betterhover:dark:hover:bg-gray-300 betterhover:hover:bg-gray-700 md:py-3 md:text-lg md:px-10 md:leading-6 font-bold"
-            >
-              <PlayIcon className="w-5" />
-              Why Million
-            </button>
-          </div>
-          <div className="relative mt-3 rounded-md sm:mt-0 sm:ml-3">
-            <Link href="/docs/start-here">
-              <a className="flex items-center justify-center w-full px-8 py-3 text-md font-medium text-black no-underline bg-gray-200 border border-transparent rounded-md dark:bg-gray-800 dark:text-white betterhover:dark:hover:bg-gray-700 betterhover:hover:bg-gray-300 md:py-3 md:text-lg md:px-10 md:leading-6">
-                Get started →
-              </a>
-            </Link>
-          </div>
-        </div>
-
-        <div className="grid-light dark:grid-dark">
-          <div className="base-light dark:base-dark"></div>
-          {Array.from({ length: 10 }, (_, i) => (
-            <div className="grid-line" style={{ '--i': i }} />
-          ))}
-        </div>
-      </div>
-
-      <div className="py-16">
-        <div className="max-w-4xl px-4 mx-auto sm:px-6 lg:px-8">
-          <p className="text-sm font-semibold tracking-wide text-center text-gray-400 text-opacity-50 uppercase dark:text-gray-500">
-            Sponsored by
+      <div className="hero">
+        <div className="px-4 pt-20 pb-8 sm:px-6 sm:pt-24 lg:px-8 dark:text-white">
+          <h1 className="text-center text-6xl font-extrabold tracking-tighter leading-[1.1] sm:text-7xl lg:text-8xl xl:text-8xl">
+            Virtual DOM
+            <br />
+            <br className="hidden lg:block" />
+            <span className="future inline-block text-transparent bg-clip-text bg-gradient-radial from-color2 to-color2 via-color1 animate-gradient-x bg-repeat">
+              into the future.
+            </span>{' '}
+          </h1>
+          <p className="max-w-lg mx-auto mt-6 text-3xl leading-tight text-center text-gray-400 sm:max-w-3xl sm:text-3xl md:text-3xl lg:text-3xl">
+            Million is a lightweight, compiler-augmented virtual DOM.
           </p>
+          <div className="max-w-xl mx-auto mt-5 sm:flex sm:justify-center md:mt-8">
+            <div className="rounded-md">
+              <button
+                onClick={() => setOpen(true)}
+                className="flex items-center justify-center w-full px-8 py-3 text-md text-white no-underline border border-transparent rounded-md bg-gradient-radial from-color2 to-color2 via-color1 animate-gradient-x bg-repeat dark:text-black betterhover:dark:hover:bg-gray-300 betterhover:hover:bg-gray-700 md:py-3 md:text-lg md:px-10 md:leading-6 font-bold"
+              >
+                <PlayIcon className="w-5" />
+                Why Million
+              </button>
+            </div>
+            <div className="relative mt-3 rounded-md sm:mt-0 sm:ml-3">
+              <Link href="/docs/start-here">
+                <a className="flex items-center justify-center w-full px-8 py-3 text-md font-medium text-black no-underline bg-gray-200 border border-transparent rounded-md dark:bg-gray-800 dark:text-white betterhover:dark:hover:bg-gray-700 betterhover:hover:bg-gray-300 md:py-3 md:text-lg md:px-10 md:leading-6">
+                  Get started →
+                </a>
+              </Link>
+            </div>
+          </div>
 
-          <div className="mt-6">
-            <div className="flex justify-center filter contrast-50 grayscale dark:opacity-50">
-              <Vercel />
+          <div className="grid-light dark:grid-dark">
+            <div className="base-light dark:base-dark"></div>
+            {Array.from({ length: 10 }, (_, i) => (
+              <div className="grid-line" style={{ '--i': i }} />
+            ))}
+          </div>
+        </div>
+
+        <div className="py-16">
+          <div className="max-w-4xl px-4 mx-auto sm:px-6 lg:px-8">
+            <p className="text-sm font-semibold tracking-wide text-center text-gray-400 text-opacity-50 uppercase dark:text-gray-500">
+              Sponsored by
+            </p>
+
+            <div className="mt-6">
+              <div className="flex justify-center filter contrast-50 grayscale dark:opacity-50">
+                <Vercel />
+              </div>
             </div>
           </div>
         </div>
@@ -164,7 +166,7 @@ export default function Page() {
               >
                 <div>
                   <feature.icon
-                    className="h-9 w-9 dark:text-white rounded-full p-1.5 dark:bg-white dark:bg-opacity-10 bg-black bg-opacity-5 text-black"
+                    className="h-9 w-9 rounded-full p-1.5 bg-gradient-to-r from-color1 to-color2 bg-opacity-50 text-white"
                     aria-hidden="true"
                   />
                 </div>
